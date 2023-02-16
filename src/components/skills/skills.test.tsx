9cd6ea8ade53d88450021192f06055eda2de0735
Palak Bansal <palakbansal3233@gmail.com>
2023-02-16 13:17:27 +0530

    complete RTL queries

--- src/components/skills/skills.test.tsx
+++ src/components/skills/skills.test.tsx
@@ -17,4 +17,32 @@ describe('Skills', () => {
     const listItemElements = screen.getAllByRole('listitem');
     expect(listItemElements).toHaveLength(skills.length);
   })
+
+  test('renders Login Button', () => {
+    render(<Skills skills={skills} />)
+    const loginButton = screen.getByRole('button', {
+      name: 'Login'
+    })
+    expect(loginButton).toBeInTheDocument();
+  })
+
+  test('Start learning button is not rendered', () => {
+    render(<Skills skills={skills} />)
+    const startLearningButton = screen.queryByRole('button', { // to test when something is not present in the DOM
+      name: 'Start learning'
+    })
+    expect(startLearningButton).not.toBeInTheDocument();
+  })
+
+  test('Start learning button is eventually displayed', async () => {
+    render(<Skills skills={skills} />)
+    const startLearningButton = await screen.findByRole('button', {
+      name: 'Start learning'
+    },
+    {
+      timeout: 2000
+    }
+    )
+    expect(startLearningButton).toBeInTheDocument();
+  })
 })
