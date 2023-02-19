db0ed992a35e64b17fb97e0c88171ad5215fd663
Palak Bansal <palakbansal3233@gmail.com>
2023-02-19 18:31:50 +0530

    keyboard interactions

---
+++ src/components/counter/counter.test.tsx
@@ -0,0 +1,97 @@
+import { render, screen } from '@testing-library/react';
+import user from '@testing-library/user-event';
+import { Counter } from './counter';
+
+describe('Counter', () => {
+  test('renders correctly', () => {
+    render(<Counter />)
+
+    const countElement = screen.getByRole('heading');
+    expect(countElement).toBeInTheDocument();
+
+    const incrementButton = screen.getByRole('button', {
+      name: 'Increment'
+    })
+    expect(incrementButton).toBeInTheDocument();
+  })
+
+  test('renders a count of zero', () => {
+    render(<Counter />);
+    const countElement = screen.getByRole('heading');
+    expect(countElement).toHaveTextContent('0');
+  })
+
+  test('renders a count of 1 after clicking the increment button', async () => {
+    user.setup()
+    render(<Counter />);
+    const incrementButton = screen.getByRole('button', {
+      name: 'Increment'
+    })
+    await user.click(incrementButton)
+    const countElement = screen.getByRole('heading');
+    expect(countElement).toHaveTextContent('1');
+  })
+
+  test('renders a count of 2 after clicking the increment button twice', async () => {
+    user.setup()
+    render(<Counter />)
+    const incrementButton = screen.getByRole('button', { name: 'Increment' })
+    await user.dblClick(incrementButton)
+    // await user.click(incrementButton)
+    // await user.click(incrementButton)
+    const countElement = screen.getByRole('heading')
+    expect(countElement).toHaveTextContent('2')
+  })
+
+  test('rendres a count of 10 after clicking the set button', async () => {
+    user.setup()
+    render(<Counter />)
+    const amountInput = screen.getByRole('spinbutton')
+    await user.type(amountInput, '10')
+    expect(amountInput).toHaveValue(10)
+    const setButton = screen.getByRole('button', { name: 'Set' })
+    await user.click(setButton)
+    const countElement = screen.getByRole('heading')
+    expect(countElement).toHaveTextContent('10')
+  })
+
+  test('elements are focused in the right order', async () => {
+    user.setup()
+    render(<Counter />)
+    const amountInput = screen.getByRole('spinbutton')
+    const setButton = screen.getByRole('button', { name: 'Set' })
+    const incrementButton = screen.getByRole('button', { name: 'Increment' })
+    await user.tab()
+    expect(incrementButton).toHaveFocus()
+    await user.tab()
+    expect(amountInput).toHaveFocus()
+    await user.tab()
+    expect(setButton).toHaveFocus()
+  })
+
+
+})
+
+/* Pointer Interactions: 
+1. click()
+2. dblClick()
+3. tripleClick()
+4. hover()
+5. unhover()
+*/
+
+/* Keybpard Interactions:
+1. Utility API: type(), clear(), selectOptions(), deselectOptions(), upload()
+2. Convenience API: tab()
+*/
+
+/* Clipboard APIs
+1. copy()
+2. cut()
+3. paste()
+*/
+
+/* Keyboard APIs
+1. keyboard('car') // translates to: c, a, r
+2. keyboard('{Shift>}A{/Shift}') // translates to: Shift(down), A, Shift(up)
+*/
