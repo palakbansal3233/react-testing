3d00b6588e4dcc5f96151eff951d5fe947fde1a8
Palak Bansal <palakbansal3233@gmail.com>
2023-02-20 13:02:23 +0530

    custom provider functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import './App.css';
-import { Application } from "./components/application/application";
+import { Application } from './components/application/application';
+import { AppProviders } from './providers/app-providers';
+import { MuiMode } from "./components/mui/mui-mode";
 
 function App() {
   return (
-    <div className="App">
-      <Application />
-    </div>
+    <AppProviders>
+      <div className="App">
+        <Application />
+        <MuiMode />
+      </div>
+    </AppProviders>
   );
 }
 
